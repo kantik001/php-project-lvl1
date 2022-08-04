63e6d907c233ce9914f8e32f4ebd07af13b93816
kantik001 <kantik001@yandex.ru>
2022-08-04 14:21:41 +0300

    step 9

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -8,8 +8,15 @@ const GAME_TASKK = "Answer 'yes' if given number is prime. Otherwise answer 'no'
 
 function findPrime($num): bool
 {
-	$result = gmp_prob_prime($num);
-	return $result;
+	if($num < 2) {
+		return false;
+	}
+	for ($i = 2; $i <= sqrt($num); $i ++) {
+		if ($number % $i == 0) {
+			return false;
+		}
+	}
+	return true;
 }
 
 function startPrimeGame()
