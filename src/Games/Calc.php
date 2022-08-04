b0e7e207b91c3ee4d71edcff6f85b65ad3d7791c
kantik001 <kantik001@yandex.ru>
2022-08-04 17:45:55 +0300

    fix

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -7,7 +7,7 @@ use function Brain\Games\Engine\runGame;
 const GAME_TASK = 'What is the result of the expression?';
 const OPERATORS = ["+", "-", "*"];
 
-function calculate($operator, $num1, $num2): string
+function calculate(string $operator,int $num1, int $num2)
 {
     switch ($operator) {
         case "+":
