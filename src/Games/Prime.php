6997edaf136cb04f86f5d5dd9e073ad91a6163b9
kantik001 <kantik001@yandex.ru>
2022-08-04 15:25:58 +0300

    fix

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -8,17 +8,19 @@ const GAME_TASKK = "Answer 'yes' if given number is prime. Otherwise answer 'no'
 
 function findPrime($num): bool
 {
-    if ($num <= 1) {
-        return false;
-    }
-    for ($i = 2; $i <= $num / 2; $i++) {
-        if ($number % $i === 0) {
-            return false;
+    if ($number <= 1 || $number % 2 === 0 || $number % 3 === 0) {
+                return false;
+    } elseif ($number === 2 || $number === 3) {
+            return true;
+    } else {
+        for ($i = 5; $i < $number; $i++) {
+            if ($number % $i === 0) {
+                return false;
+            }
         }
+                    return true;
     }
-    return true;
 }
-
 function startPrimeGame()
 {
     $getGameData = function () {
